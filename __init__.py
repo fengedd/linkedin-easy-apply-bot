280f4aa53177eeca5ccdaf4392ac4a50c8ca3e83
unknown <fengedd@gmail.com>
2018-12-25 12:33:44 -0800

    Added driver wait and linkedin filter

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 
 print("Webdriver Program Initiated... \n")
@@ -8,6 +9,8 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://linkedin.com")
 job_title = "Software Engineer"
 location = "United States"
+
+wait= WebDriverWait(driver,1)
 assert "LinkedIn" in driver.title
 try: 
     #Assert
@@ -39,15 +42,25 @@ try:
     location_search_box_input.clear()
     location_search_box_input.send_keys(location)
     location_search_box_input.send_keys(Keys.RETURN)
+    
 
-    #Assert
-    print("button[data-control-name=\"all_filters\"]")
-    filter_button = driver.find_element_by_css_selector("button.search-filters-bar__all-filters.artdeco-button.artdeco-button--muted.artdeco-button--2.artdeco-button--tertiary.ember-view")
+    #Assert    
+    driver.implicitly_wait(10)
+    filter_button = driver.find_element_by_css_selector("button[data-control-name=\"all_filters\"]")
     filter_button.click()
+
+    filter_form = driver.find_element_by_css_selector("ul.search-advanced-facets__facets-list")
+    linkedin_features_form = filter_form.find_element_by_css_selector("div#linkedin-features-facet-values")
+    date_posted_form = filter_form.find_element_by_css_selector("#date-posted-facet-values")
+    job_type_form = filter_form.find_element_by_css_selector("#job-type-facet-values")     
+    experience_level_form = filter_form.find_element("#experience-level-facet-values")
+
+
+
     
     
     
     
 except NoSuchElementException:
     print('Exception!')
-    driver.close()
+   
