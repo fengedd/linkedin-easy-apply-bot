a6d7dab464ddde64bad8cffc7299ce2adc0befe9
unknown <fengedd@gmail.com>
2018-12-26 23:21:17 -0800

    SignInPage class for Page-Object pattern

--- __init__.py
+++ __init__.py
@@ -4,28 +4,35 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 
+
+class SignInPage:
+    def __init__(self):
+        self.email_input = login_form.find_element_by_css_selector("input#login-email.login-email")
+        self.pw_input = login_form.find_element_by_css_selector("input#login-password.login-password")
+
+
 print("Webdriver Program Initiated... \n")
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://linkedin.com")
 job_title = "Software Engineer"
 location = "United States"
 
-wait= WebDriverWait(driver,1)
+
 assert "LinkedIn" in driver.title
 try: 
     #Assert
     login_form = driver.find_element_by_class_name("login-form")
     
-    email_input = login_form.find_element_by_css_selector("input#login-email.login-email")    
-    email_input.clear()
-    email_input.send_keys(email)
+    sp = SignInPage()
+   
+    #email_input = login_form.find_element_by_css_selector("input#login-email.login-email")    
+    sp.email_input.clear()
+    sp.email_input.send_keys(email)
     
-    
-    pw_input = login_form.find_element_by_css_selector("input#login-password.login-password")
-    pw_input.clear()
-    pw_input.send_keys(pw)
-    
-    email_input.send_keys(Keys.RETURN)
+    #pw_input = login_form.find_element_by_css_selector("input#login-password.login-password")
+    sp.pw_input.clear()
+    sp.pw_input.send_keys(pw)    
+    sp.email_input.send_keys(Keys.RETURN)
 
     #Assert
     jobs_button = driver.find_element_by_css_selector("li#jobs-nav-item.nav-item.nav-item--jobs")
@@ -48,12 +55,38 @@ try:
     driver.implicitly_wait(10)
     filter_button = driver.find_element_by_css_selector("button[data-control-name=\"all_filters\"]")
     filter_button.click()
-
+    driver.implicitly_wait(10)
     filter_form = driver.find_element_by_css_selector("ul.search-advanced-facets__facets-list")
-    linkedin_features_form = filter_form.find_element_by_css_selector("div#linkedin-features-facet-values")
+    linkedin_features_form_inputs = filter_form.find_element_by_css_selector("div#linkedin-features-facet-values").find_elements_by_css_selector("li")
     date_posted_form = filter_form.find_element_by_css_selector("#date-posted-facet-values")
     job_type_form = filter_form.find_element_by_css_selector("#job-type-facet-values")     
-    experience_level_form = filter_form.find_element("#experience-level-facet-values")
+    experience_level_form_inputs = filter_form.find_element_by_css_selector("#experience-level-facet-values").find_elements_by_css_selector("li")
+    driver.implicitly_wait(10)
+    easy_apply_label = linkedin_features_form_inputs[0] 
+    in_network_label = linkedin_features_form_inputs[1]
+    under_10_applicants_label = linkedin_features_form_inputs[2]
+    driver.implicitly_wait(10)
+    easy_apply_label.click()
+    driver.implicitly_wait(10)
+    internship_label = experience_level_form_inputs[0] 
+    entry_level_label = experience_level_form_inputs[1] 
+    associate_label = experience_level_form_inputs[2] 
+    driver.implicitly_wait(10)
+    internship_label.click()
+    entry_level_label.click()
+    associate_label.click()
+
+    apply_filter_button = driver.find_element_by_css_selector("button.search-advanced-facets__button--apply.button-primary-large")
+    apply_filter_button.click()
+
+
+
+    job_display_panes = driver.find_element_by_css_selector("div.jobs-search-two-pane__wrapper.jobs-search-two-pane__wrapper--two-pane")
+    search_results_pane = job_display_panes.find_element("div.jobs-search-results.jobs-search-results--is-two-pane")
+
+
+
+
 
 
 
